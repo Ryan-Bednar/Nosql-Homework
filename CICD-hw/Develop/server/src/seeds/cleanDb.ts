5d4a7da73c2b825aef54d0cc2fd6808a499c147d
Ryan Bednar <rbednar01@gmail.com>
2025-03-05 20:13:43 -0600

    Added Code and YAML

---
+++ CICD-hw/Develop/server/src/seeds/cleanDb.ts
@@ -0,0 +1,18 @@
+import models from '../models/index.js';
+import db from '../config/connection.js';
+export default async (modelName: "Question", collectionName: string) => {
+  try {
+    const model = models[modelName];
+    if (!model || !model.db || !model.db.db) {
+      throw new Error(`Model or database not found for modelName: ${modelName}`);
+    }
+    const modelExists = await model.db.db.listCollections({
+      name: collectionName
+    }).toArray();
+    if (modelExists.length) {
+      await db.dropCollection(collectionName);
+    }
+  } catch (err) {
+    throw err;
+  }
+}
